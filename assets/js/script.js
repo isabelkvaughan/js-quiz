cb25fc2f3b439af54639d6d4a31db345e152b8f4
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-03-17 15:33:19 +1100

    js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,62 @@
 //questions
-var questions = [
-{
-  question: "Question 1",
-  answers: ["A", "B", "C", "D"],
-  correctAnswer: "A",
-},
-{
-  question: "Question 2",
-  answers: ["A", "B", "C", "D"],
-  correctAnswer: "B",
-},
-{
-  question: "Question 3",
-  answers: ["A", "B", "C", "D"],
-  correctAnswer: "C",
-},
-{
-  question: "Question 4",
-  answers: ["A", "B", "C", "D"],
-  correctAnswer: "D",
-},
-{
-  question: "Question 5",
-  answers: ["A", "B", "C", "D"],
-  correctAnswer: "A",
-},
+var quizQuestions = [
+  {
+    question: "Question 1",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: "A",
+  },
+  {
+    question: "Question 2",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: "B",
+  },
+  {
+    question: "Question 3",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: "C",
+  },
+  {
+    question: "Question 4",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: "D",
+  },
+  {
+    question: "Question 5",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: "A",
+  },
 ];
 
 //startQuiz
-
 var startQuiz = function() {
   console.log('starting quiz');
-  
-}
+  var currentQuestionIndex = 0;
+  var question = quizQuestions[currentQuestionIndex];
+  var questionElement = document.createElement("div");
+  questionElement.textContent = question.question;
+  document.getElementById("questions").appendChild(questionElement);
+
+  for (var i = 0; i < question.answers.length; i++) {
+    var choice = question.answers[i];
+    var choiceElement = document.createElement("button");
+    choiceElement.textContent = choice;
+    document.getElementById("questions").appendChild(choiceElement);
+
+    choiceElement.addEventListener("click", (event) => {
+      if (event.target.textContent === question.correctAnswer) {
+        var feedbackElement = document.createElement("div");
+        feedbackElement.textContent = "Correct!";
+        document.getElementById("questions").appendChild(feedbackElement);
+      } else {
+        var feedbackElement = document.createElement("div");
+        feedbackElement.textContent = "Incorrect!";
+        document.getElementById("questions").appendChild(feedbackElement);
+        timeLeft = timeLeft - 10;
+      }      
+    });
+  }
+};  
+
 
 //intro screen
 var introScreen = document.getElementById("intro");

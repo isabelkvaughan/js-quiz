86452fc6447c0a519387c598cc34587b754be18a
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-03-17 14:19:29 +1100

    timer fixed and sample questions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,64 @@
-//start button
+//questions
+var questions = [
+{
+  question: "Question 1",
+  answers: ["A", "B", "C", "D"],
+  correctAnswer: "A",
+},
+{
+  question: "Question 2",
+  answers: ["A", "B", "C", "D"],
+  correctAnswer: "B",
+},
+{
+  question: "Question 3",
+  answers: ["A", "B", "C", "D"],
+  correctAnswer: "C",
+},
+{
+  question: "Question 4",
+  answers: ["A", "B", "C", "D"],
+  correctAnswer: "D",
+},
+{
+  question: "Question 5",
+  answers: ["A", "B", "C", "D"],
+  correctAnswer: "A",
+},
+];
 
-var startButton = document.getElementById("start-button");
+//startQuiz
+
+var startQuiz = function() {
+  console.log('starting quiz');
+  
+}
+
+//intro screen
+var introScreen = document.getElementById("intro");
+var introHeading = document.createElement("h1");
+introHeading.textContent = "JavaScript Fundamentals: Multiple Choice";
+introScreen.appendChild(introHeading);
+
+var introParagraph = document.createElement("p");
+introParagraph.textContent = "Answer the following questions on JavaScript Fundamentals, within the time limit.";
+introScreen.appendChild(introParagraph);
+
+//start button
+var startButton = document.createElement("button");
+startButton.textContent = "START QUIZ";
+introScreen.appendChild(startButton);
+
 startButton.addEventListener("click", () => {
   console.log('test');
+  introScreen.remove();
   startTimer();
+  startQuiz();
 });
 
 
 //timer
-
-var timeLeft = 60;
+var timeLeft = 90;
 var timerInterval;
 
 function startTimer() {
@@ -21,3 +70,5 @@ function startTimer() {
     }
   }, 1000);
 }
+
+
